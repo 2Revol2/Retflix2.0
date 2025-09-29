c93cc032eb2ba04e5d13f624046cb2868e22747a
2Revol2 <maksimbaluta02@gmail.com>
2025-09-29 17:25:18 +0200

    fix: fix sidebar mobile width and getMoviesPremieres

--- src/shared/api/axios/premieres/api.ts
+++ src/shared/api/axios/premieres/api.ts
@@ -6,7 +6,7 @@ export const getMoviesPremieres = async () => {
   const year = new Date().getFullYear();
   const month = monthArray[new Date().getMonth()];
   return (
-    await baseInstance.get<PremiresResponse>("/v2.2/films/premieres", {
+    await baseInstance.get<PremiresResponse>("v2.2/films/premieres", {
       params: {
         year,
         month,
