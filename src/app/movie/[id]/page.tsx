9935ca431c75ed32a8b68b4442eeb789d73e2add
2Revol2 <maksimbaluta02@gmail.com>
2025-09-30 09:02:33 +0200

    feat: add translations on Movie Details page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { getTranslations } from "next-intl/server";
 import { getFilm } from "@/shared/api/axios/movie/api";
 import { HStack, VStack } from "@/shared/ui/Stack";
 import { MovieInfo } from "@/entities/Movie";
@@ -7,6 +8,7 @@ import s from "./movie.module.css";
 const MovieDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const movie = await getFilm(id);
+  const t = await getTranslations("MovieDetails");
 
   return (
     <VStack gap="16">
@@ -18,23 +20,23 @@ const MovieDetails = async ({ params }: { params: Promise<{ id: string }> }) => 
           <h3 className={s.title}>{movie.nameRu}</h3>
           <HStack gap="32">
             <p className={s.rating}>
-              <span className={s.kinopoisk}>КиноПоиск:</span> {movie.ratingKinopoisk}
+              <span className={s.kinopoisk}>{t("kinopoisk")}:</span> {movie.ratingKinopoisk}
             </p>
             <p className={s.rating}>
-              <span className={s.imdb}>IMDB:</span> {movie.ratingImdb}
+              <span className={s.imdb}>{t("imdb")}:</span> {movie.ratingImdb}
             </p>
           </HStack>
           <div className={s.infoGrid}>
-            <MovieInfo title={"Год"} info={movie.year} />
-            <MovieInfo title={"Страна"} info={movie.countries.map((c) => c.country)} />
-            <MovieInfo title={"Жанр"} info={movie.genres.map((g) => g.genre)} />
-            {movie.filmLength && <MovieInfo title={"Длительность"} info={`${movie.filmLength} мин.`} />}
+            <MovieInfo title={t("year")} info={movie.year} />
+            <MovieInfo title={t("country")} info={movie.countries.map((c) => c.country)} />
+            <MovieInfo title={t("genre")} info={movie.genres.map((g) => g.genre)} />
+            {movie.filmLength && <MovieInfo title={t("duration")} info={`${movie.filmLength} ${t("min")}`} />}
           </div>
-          <MovieInfo title={"Описание"} info={movie.description} />
+          <MovieInfo title={t("description")} info={movie.description} />
         </VStack>
       </div>
       <VStack max align="center" gap="16">
-        <h3>Смотреть онлайн</h3>
+        <h3>{t("watchOnline")}</h3>
         <VideoPlayer />
       </VStack>
     </VStack>
