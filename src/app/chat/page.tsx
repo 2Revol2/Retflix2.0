d78915130b23469183a018b06f1d0b2e3d6aefb7
2Revol2 <maksimbaluta02@gmail.com>
2025-10-08 13:34:45 +0200

    feat: add auth0

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,18 @@
-const Chat = () => {
+import Link from "next/link";
+import { auth0 } from "@/shared/lib/auth0";
+
+const Chat = async () => {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <div>
+        <Link href="/auth/login?screen_hint=signup">Sign up</Link>
+        <Link href="/auth/login">Log in</Link>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h5>Chat</h5>
